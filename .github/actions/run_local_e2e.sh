9559978643db3198713640ef773a1f15aeb72188
lezhumain <lezzhumain@gmail.com>
2021-12-28 23:10:36 +0000

    optimize e2e action script

--- .github/actions/run_local_e2e.sh
+++ .github/actions/run_local_e2e.sh
@@ -10,36 +10,33 @@ curl -o SplitMan2-API.zip https://codeload.github.com/lezhumain/SplitMan2-API/zi
 #curl -o SplitMan2.zip https://codeload.github.com/lezhumain/SplitMan2/zip/refs/heads/version_1
 unzip SplitMan2-API.zip
 
+echo "Build APP and run Angular server"
+npm ci
+#npm run build:prod
+npx ng serve &
+
+echo "Build API and run"
 cd SplitMan2-API-*
 #mvn clean validate compile compiler:testCompile test package -f pom.xml
 mvn package -f pom.xml
 
 cd target
-
 TARGET_JAR="$(ls demo-*.*.*-SNAPSHOT.jar | head -n 1)"
 
-#ls
-#cp "$TARGET_JAR" ../
+java -jar "$TARGET_JAR" --server.port=8888 &
 
-java  -jar "$TARGET_JAR" --server.port=8888 &
-
+echo "Waiting for API server..."
 "$WORKING_DIR"/waitForServer.sh 127.0.0.1:8888
+echo "API server running."
+
+echo "Waiting for Angular server..."
+"$WORKING_DIR"/waitForServer.sh 127.0.0.1:4200
+echo "Angular server running."
 
 SERVER_PID="$(ps -fu $USER| grep "[d]emo" | awk '{print $2}')"
-
-
-## SETUP app
-cd "$WORKING_DIR"
-npm ci
-#npm run build:prod
-npx ng serve &
-
-echo "waitForServer.sh..."
-"$WORKING_DIR"/waitForServer.sh 127.0.0.1:4200
-echo "ok"
-
 APP_PID="$(ps -fu $USER| grep "[n]g serve" | awk '{print $2}')"
 
+cd "$WORKING_DIR"
 
 npm run e2e
 RES="$?"
