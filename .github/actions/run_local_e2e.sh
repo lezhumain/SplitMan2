f79880d2a813eaf96f22112f6969ecf220ba734f
lezhumain <lezzhumain@gmail.com>
2021-12-28 23:17:32 +0000

    use correct api branch

--- .github/actions/run_local_e2e.sh
+++ .github/actions/run_local_e2e.sh
@@ -6,7 +6,7 @@ WORKING_DIR="$(pwd)"
 
 ## SETUP API
 # get api
-curl -o SplitMan2-API.zip https://codeload.github.com/lezhumain/SplitMan2-API/zip/refs/heads/master
+curl -o SplitMan2-API.zip https://codeload.github.com/lezhumain/SplitMan2-API/zip/refs/heads/master_1
 #curl -o SplitMan2.zip https://codeload.github.com/lezhumain/SplitMan2/zip/refs/heads/version_1
 unzip SplitMan2-API.zip
 
