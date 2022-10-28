cae4f9b5f945d1f571723d5aa8373ea1afe19fd8
lezhumain <julien.merion@whitespace.co.uk>
2022-10-28 22:07:59 +0100

    sed for macOS

--- doBuild.sh
+++ doBuild.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 
+function sedi()
+{
+  if [ -z "$(uname -a | grep -i darwin)" ]; then
+    #linux
+    sed -i .bak $@
+  else
+    # macos
+    sed -i".bak" $@
+  fi
+}
+
 # change IP
-sed -i .bak "s|PROD_IP|$1|g" src/environments/environment.prod.ts
+#sed -i .bak "s|PROD_IP|$1|g" src/environments/environment.prod.ts
+sedi "s|PROD_IP|$1|g" src/environments/environment.prod.ts
 
 docker build -t splitman2 .
