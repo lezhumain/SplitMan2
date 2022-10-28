74f6fc4dc84d515fa292f8ba700518219cf968d7
lezhumain <julien.merion@whitespace.co.uk>
2022-10-28 22:47:14 +0100

    try fix sedi

--- doBuild.sh
+++ doBuild.sh
@@ -4,15 +4,16 @@ function sedi()
 {
   if [ -z "$(uname -a | grep -i darwin)" ]; then
     #linux
-    sed -i .bak $1 $2 $3
+    sed -i.bak "$1" "$2" "$3"
+    #sed -i.bak "s|PROD_IP|$1|g" src/environments/environment.prod.ts
   else
     # macos
-    sed -i".bak" $1 $2 $3
+    sed -i ".bak" $1 $2 $3
   fi
 }
 
 # change IP
-#sed -i .bak "s|PROD_IP|$1|g" src/environments/environment.prod.ts
-sedi "s|PROD_IP|$1|g" src/environments/environment.prod.ts
+#sed -i.bak -e "s|PROD_IP|$1|g" src/environments/environment.prod.ts
+sedi -e "s|PROD_IP|$1|g" src/environments/environment.prod.ts
 
 docker build -t splitman2 .
