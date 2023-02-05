b1100224c317b9d256b559eaa6fd2c69afdd1809
lezhumain <julien.merion@whitespace.co.uk>
2023-02-05 04:06:30 +0000

    add headless arg if headless

--- e2e/e2e_utils.ts
+++ e2e/e2e_utils.ts
@@ -35,6 +35,7 @@ export async function CreateBrowsers(): Promise<[Browser, Browser]> {
     args: [
       '--start-maximized',
       '--no-sandbox', // discouraged, see https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
+      // '--disable-setuid-sandbox',
       '--window-size=1920,978',
       '--disable-web-security',
       '--disable-features=IsolateOrigins',
@@ -43,6 +44,10 @@ export async function CreateBrowsers(): Promise<[Browser, Browser]> {
     // executablePath: "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
     // executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
   };
+
+  if(isHeadless) {
+    pupArgs.args.push("--headless");
+  }
 
   const browser = await puppeteer.launch(pupArgs),
         browser1 = await puppeteer.launch(JSON.parse(JSON.stringify(pupArgs)));
