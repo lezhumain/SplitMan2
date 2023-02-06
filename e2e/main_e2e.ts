6be1aa9b21d04bae0abe34ca7d7c04826cd59d8b
lezhumain <julien.merion@whitespace.co.uk>
2023-02-06 00:19:20 +0000

    comment-out register

--- e2e/main_e2e.ts
+++ e2e/main_e2e.ts
@@ -1438,19 +1438,19 @@ async function runAll() {
     //   msg: "Test Google",
     //   params: []
     // },
-    {
-      fn: MainRegister,
-      msg: "E2E register",
-      params: []
-    },
     // {
-    //   fn: MainTest,
-    //   msg: "E2E with 1 expense",
-    //   params: [
-    //     allExpenses.slice(0, 1),
-    //     "Dju doit a 8.56€ Suzie Max doit a 8.56€ Suzie Elyan doit a 8.56€ Suzie"
-    //   ]
+    //   fn: MainRegister,
+    //   msg: "E2E register",
+    //   params: []
     // },
+    {
+      fn: MainTest,
+      msg: "E2E with 1 expense",
+      params: [
+        allExpenses.slice(0, 1),
+        "Dju doit a 8.56€ Suzie Max doit a 8.56€ Suzie Elyan doit a 8.56€ Suzie"
+      ]
+    },
     // {
     //   fn: MainTest,
     //   msg: "E2E with all expenses",
