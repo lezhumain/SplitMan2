8edd9d6f99fd4d83af0aefe0159d21a5755bbe21
lezhumain <julien.merion@whitespace.co.uk>
2023-05-02 19:31:45 +0100

    install sdk 30

--- prepare_android.sh
+++ prepare_android.sh
@@ -30,21 +30,21 @@ function install_android_sdk() {
   export PATH=":$PATH:$ANDROID_HOME/emulator"
   export PATH=":$PATH:$ANDROID_HOME/platform-tools"
 
-	#android update sdk
-	#android list target
-	ls -al "$ANDROID_HOME"
-	echo "1"
-	ls -al "$ANDROID_HOME/cmdline-tools"
-	echo "2"
-	ls -al "$ANDROID_HOME/emulator"
-	echo "3"
-	ls -al "$ANDROID_HOME/platform-tools"
+  #android update sdk
+  #android list target
+  ls -al "$ANDROID_HOME"
+  echo "1"
+  ls -al "$ANDROID_HOME/cmdline-tools"
+  echo "2"
+  ls -al "$ANDROID_HOME/emulator"
+  echo "3"
+  ls -al "$ANDROID_HOME/platform-tools"
 
 #	echo "11"
 #	sudo find /usr/local/lib/android/sdk/ -name sdkmanager
 #	echo "12"
-#	/usr/local/lib/android/sdk/tools/bin/sdkmanager --list
-#	#sdkmanager --install "platform-tools" "build-tools;30.0.1" "emulator" "platforms:android-33"
+  sdkmanager --list
+  sdkmanager --install "platform-tools" "build-tools;30.0.1" "emulator" "platforms:android-33"
 }
 
 function install_android_sdk1() {
