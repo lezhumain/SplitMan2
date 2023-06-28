fb70347622134e6a1399e01aa9c3f8721f8b9cf5
lezhumain <julien.merion@whitespace.co.uk>
2023-06-29 00:32:18 +0100

    dockerfiles for android

--- prepare_android.sh
+++ prepare_android.sh
@@ -9,69 +9,87 @@ function install_gradle() {
 	unzip -d /opt/gradle gradle-*-bin.zip
 	ls /opt/gradle/gradle-*
 
-	export PATH="$PATH:"/opt/gradle/gradle-*/bin
+  local PATH_TO_ADD="$(ls -d /opt/gradle/gradle-*/bin | head -n 1)"
+	export PATH="$PATH:$PATH_TO_ADD"
+	echo "export PATH=\"\$PATH:$PATH_TO_ADD\"" >> ~/.bashrc
+  echo "$PATH_TO_ADD" > "/path_add_2"
 
+  echo "$PATH"
+  echo "gradle 00"
+  ls -al /opt/gradle/gradle-*/bin
+  echo "gradle version:"
 	gradle -v
+}
+
+function install_sdkmanager() {
+  mkdir -p ~/android/sdk
+  pushd ~/android/sdk
+
+  # curl https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -O
+  LATEST="$(curl -s https://developer.android.com/studio | grep -Eo "commandlinetools-linux.+\.zip" | head -n 1)"
+  curl "https://dl.google.com/android/repository/$LATEST" -O
+  unzip *.zip
+  pushd cmdline-tools
+  mkdir ../tools
+
+  mv ./* ../tools/
+  mv ../tools ./
+
+  popd
+  popd
+}
+
+function setAndroidPath() {
+    echo "finding android home"
+
+    local ANDROID_HOME0="$(dirname "$(find / -name cmdline-tools | head -n 1)")"
+    echo "ANDROID_HOME0: $ANDROID_HOME0"
+    local ANDROID_SDK_ROOT="$ANDROID_HOME"
+
+    echo "export ANDROID_HOME=\"$ANDROID_HOME0\"
+export ANDROID_SDK_ROOT=\"\$ANDROID_SDK_ROOT\"
+export PATH=\"\$PATH:\$ANDROID_HOME/tools/bin:\$ANDROID_HOME/cmdline-tools/tools/bin:\$ANDROID_HOME/platform-tools\"" >> ~/.bashrc
+
+    export ANDROID_HOME="$ANDROID_HOME0"
+    export ANDROID_SDK_ROOT="$ANDROID_SDK_ROOT"
+    PATH_TO_ADD="$ANDROID_HOME/tools/bin:$ANDROID_HOME/cmdline-tools/tools/bin:$ANDROID_HOME/platform-tools"
+    export PATH="$PATH:$PATH_TO_ADD"
+    echo "$PATH_TO_ADD" > "/path_add_1"
+
+    cat "$HOME/.bashrc"
+
+    echo "PATH: $PATH"
+    echo "ANDROID_HOME: $ANDROID_HOME"
 }
 
 function install_android_sdk() {
 	# install android sdk
-  #sudo apt install default-jdk
-	sudo apt install android-sdk
-  #sudo find / -name android
 
-  ls -ald /usr/local/lib/android
-  ls -ald /usr/local/lib/android/sdk/tools/android
-  echo "$PATH"
+  #  if [ $(getent group sudo) ] && [ -n "$(which sudo)" ]; then
+  #    #sudo apt install default-jdk
+  #    sudo apt install android-sdk -y
+  #    #sudo find / -name android
+  #  else
+  #    apt install android-sdk -y
+  #  fi
+  #
+  #  if [ -z "$(find / -name sdkmanager)" ]; then
+  #    install_sdkmanager
+  #  fi
+  install_sdkmanager
 
-  export ANDROID_HOME="/usr/local/lib/android/sdk"
-  export PATH="$PATH:$ANDROID_HOME/tools/bin"
-  export PATH=":$PATH:$ANDROID_HOME/cmdline-tools/bin"
-  export PATH=":$PATH:$ANDROID_HOME/emulator"
-  export PATH=":$PATH:$ANDROID_HOME/platform-tools"
-
-  #android update sdk
-  #android list target
-  ls -al "$ANDROID_HOME"
-  echo "1"
-  ls -al "$ANDROID_HOME/cmdline-tools"
-  echo "2"
-  ls -al "$ANDROID_HOME/emulator"
-  echo "3"
-  ls -al "$ANDROID_HOME/platform-tools"
-
-#	echo "11"
-#	sudo find /usr/local/lib/android/sdk/ -name sdkmanager
-#	echo "12"
+  setAndroidPath
 
   echo "list...."
   sdkmanager --list
-  #  echo "install...."
-  #  sdkmanager --install "platform-tools" "build-tools;30.0.1" "emulator" "platforms:android-33"
-  #  echo "installed 1...."
+
+  echo "install...."
+#  sdkmanager --licenses
+  yes | sdkmanager --install "platform-tools" "build-tools;33.0.2" "emulator" "platforms;android-33"
+  echo "installed 1...."
 }
 
-#function install_android_sdk1() {
-#	# install android sdk 1
-#  mkdir -p ~/android/sdk
-#  cd ~/android/sdk
-#  curl https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -O
-#  sudo unzip *.zip
-#  cd cmdline-tools
-#  mkdir tools
-#  mv -i * tools
-#  export ANDROID_HOME="$HOME/android/sdk"
-#  export PATH="$ANDROID_HOME/cmdline-tools/tools/bin/:$PATH"
-#  export PATH="$ANDROID_HOME/emulator/:$PATH"
-#  export PATH="$ANDROID_HOME/platform-tools/:$PATH"
-#
-#  echo "list...."
-#  sdkmanager --list
-#  #  echo "install...."
-#  #  sdkmanager --install "platform-tools" "build-tools;30.0.1" "emulator" "platforms:android-33"
-#  #  echo "installed 1...."
-#}
-
 install_gradle
-install_android_sdk
-# install_android_sdk1
+if [ -z "$1" ]; then
+  install_android_sdk
+fi
